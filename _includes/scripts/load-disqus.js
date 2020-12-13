39954876287221d0d41b82d82b438d9199de913f
azza999 <azzadh999@gmail.com>
2020-12-13 21:49:29 +0900

    modify disqus

--- _includes/scripts/load-disqus.js
+++ _includes/scripts/load-disqus.js
@@ -30,13 +30,13 @@ window.disqus_reload = function(w, d) {
 
     loadJSDeferred('//{{ site.disqus_shortname }}.disqus.com/embed.js');
     loadDQ({});
-/*
-      if (w.addEventListener) w.addEventListener('scroll', loadDQ, { passive: true });
-      else if (w.attachEvent) w.attachEvent('onscroll', loadDQ);
-      else w.onscroll = loadDQ;
-    } else {
-      w._disqusFirst = false;
-    }*/
+
+    /*if (w.addEventListener) w.addEventListener('scroll', loadDQ, { passive: true });
+    else if (w.attachEvent) w.attachEvent('onscroll', loadDQ);
+    else w.onscroll = loadDQ;*/
+  } else {
+    w._disqusFirst = false;
+  }
 }
 
 ; window.disqus_reload(window, document);
